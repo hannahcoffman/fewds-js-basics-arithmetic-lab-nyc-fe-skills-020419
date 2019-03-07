25b13419887377bae398c266799438548ab651c2
Hannah Coffman <hcoffman7@gmail.com>
2019-03-07 02:09:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,4 +11,5 @@ let newID = (1000000000 + oldID)
 let ageIsValid = Number.isInteger(currentAge)
 let randomNumber = Math.random()
 let randomInteger = Math.round(randomNumber)
-let randomUserId = Number.isInteger(1000000001,1000000020)
+let randomUserId 
+(1000000001,1000000020)
