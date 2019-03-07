632009eba0ab9ff74cab3b06e79b398cb7b7f5b8
Hannah Coffman <hcoffman7@gmail.com>
2019-03-07 01:50:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ You should NOT try to redefine any of the above variables.
 let newID = (1000000000 + oldID)
 let ageIsValid = Number.isInteger(currentAge)
 let randomNumber = Math.random()
+let radomInteger = 
 const flooredRandomInteger = Math.floor(randomNumber);
 expect(randomInteger).to.eql(flooredRandomInteger);
 
